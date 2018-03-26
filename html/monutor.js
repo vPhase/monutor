6f084b68e0e20cbd74c72c6b2d0c42b6c647cdeb
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-03-26 17:36:38 -0500

    add status support, custom colors, load each file individually

--- html/monutor.js
+++ html/monutor.js
@@ -1,6 +1,5 @@
 
-
-
+var graph_colors = [40,46,30,6,,2,4,7,9,1,25,12,17,33,42,49,41,28,20]; 
 
 
 function optClear()
@@ -16,7 +15,7 @@ function optAppend(str)
 
 function plotHelp()
 {
-alert("||| to separate lines on same plot, line returns to separate plots"); 
+alert("Eventually this will document what to do!"); 
 
 }
 
@@ -31,51 +30,65 @@ function lastModified(file)
   return new Date(req.getResponseHeader("Last-Modified")).getTime(); 
 }
 
-// persist some things... 
-var canvases = []; 
-var graphs = [];  
-var multigraphs = []; 
-var legends = []; 
-var wants = []; 
-var labels =[]; 
-var xtitles = []; 
-var ytitles = []; 
-var titles = []; 
-var xtime = []; 
-var ytime = []; 
-var pstyle = []; 
+var pages = {}; 
 
-function clearCanvases()
+function Page(name)
 {
-  graphs = []; 
-  wants = [];  
-  multigraphs = []; 
-  xtitles = []; 
-  ytitles = []; 
-  xtime = []; 
-  ytime = []; 
-  labels = []; 
-  pstyle = []; 
-  legends = []; 
-  titles = []; 
+  console.log("Made new page " + name); 
+  P = new Object; 
+  P.main_canvas = name+"c"; 
+  document.body.innerHTML += '<div id="'+P.main_canvas+'" style="display: none" width="100%" height=100%"> (loading js...)'+name+' </div>'; 
+  P.page_name = name; 
+  P.canvases = []; 
+  P.graphs = [];  
+  P.leg_graphs = [];  
+  P.multigraphs = []; 
+  P.legends = []; 
+  P.wants = []; 
+  P.labels =[]; 
+  P.xtitles = []; 
+  P.ytitles = []; 
+  P.titles = []; 
+  P.xtime = []; 
+  P.ytime = []; 
+  P.pstyle = []; 
+//  console.log(P); 
+  return P; 
+}
 
-  for (var i = 0; i < canvases.length; i++) 
+// persist some things... 
+function clearCanvases(p)
+{
+  p.graphs = []; 
+  p.leg_graphs = []; 
+  p.wants = [];  
+  p.multigraphs = []; 
+  p.xtitles = []; 
+  p.ytitles = []; 
+  p.xtime = []; 
+  p.ytime = []; 
+  p.labels = []; 
+  p.pstyle = []; 
+  p.legends = []; 
+  p.titles = []; 
+
+  for (var i = 0; i < p.canvases.length; i++) 
   {
-    JSROOT.cleanup(canvases[i]); 
+      JSROOT.cleanup(p.canvases[i]); 
   }
 
-  canvases =[]; 
-  var c = document.getElementById('c'); 
+  p.canvases =[]; 
+  var c = document.getElementById(p.main_canvas); 
   c.innerHTML = ""; 
 }
 
-function addCanvas() 
+function addCanvas(P) 
 {
-  var i = canvases.length+1; 
-  name = "c" + i; 
-  var c = document.getElementById('c'); 
+  var i = P.canvases.length+1; 
+  var name = P.page_name+"_c" + i; 
+  var c = document.getElementById(P.main_canvas); 
   c.innerHTML += '<div class="canvas" id="' +name+ '">'+name+'</div>'; 
-  canvases.push(name); 
+  P.canvases.push(name); 
   return name; 
 }
 
@@ -102,7 +115,7 @@ function makeLegend(xlow,xhigh,ylow,yhigh, objs)
       leg.fY2NDC = yhigh; 
       leg.fFillStyle=1001; 
       leg.fFillColor=36; 
-      leg.fNColumns = objs.length > 3 ? 2 : 1; 
+      leg.fNColumns = objs.length > 12 ? 4 : objs.length > 8 ? 3 : objs.length > 3 ? 2 : 1; 
  
       for (var i = 0; i < objs.length; i++) 
       {
@@ -118,9 +131,10 @@ function makeLegend(xlow,xhigh,ylow,yhigh, objs)
 }
 
 
-function doDraw(t, what,cut) 
+function doDraw(page, ts, what,cut) 
 {
-  clearCanvases(); 
+
+  clearCanvases(page); 
   var plots = document.getElementById(what).value.split("\n"); 
   for (var i = 0; i < plots.length; i++) 
   {
@@ -136,7 +150,7 @@ function doDraw(t, what,cut)
     var this_label = []; 
     var this_xtime = false; 
     var this_ytime = false; 
-    var this_pstyle = "PLC"; 
+    var this_pstyle = "lp"; 
     var this_title = "Plot "+i; 
 
     if (these_plots.length > 1) 
@@ -180,82 +194,162 @@ function doDraw(t, what,cut)
 
     }
 
-    xtitles.push(this_xtitle); 
-    titles.push(this_title); 
-    ytitles.push(this_ytitle); 
-    labels.push(this_label); 
-    xtime.push(this_xtime); 
-    ytime.push(this_ytime); 
-    pstyle.push(this_pstyle); 
+    page.xtitles.push(this_xtitle); 
+    page.titles.push(this_title); 
+    page.ytitles.push(this_ytitle); 
+    page.labels.push(this_label); 
+    page.xtime.push(this_xtime); 
+    page.ytime.push(this_ytime); 
+    page.pstyle.push(this_pstyle); 
 
 
-    graphs.push([]); 
-    addCanvas(); 
-    wants.push(draws.length); 
+    page.graphs.push([]); 
+    page.leg_graphs.push([]); 
+    addCanvas(page); 
+    page.wants.push(draws.length*ts.length); 
     for (var j = 0; j < draws.length; j++) 
     {
-      args = { expr: draws[j], cut: cut, graph: true, drawopt: [i,j]}; 
-      t.Draw(args, function(g,indices,ignore)
+      for (var it = 0; it < ts.length; it++)
       {
-        var ii = indices[0]; 
-        var jj = indices[1]; 
-        g.fEditable = false; 
-        g.fTitle = labels[ii][jj]; 
-        g.fName = labels[ii][jj]; 
-        graphs[ii].push(g); 
-        if (graphs[ii].length == wants[ii]) 
+
+        if (ts[it] == null)
         {
-          var mg = JSROOT.CreateTMultiGraph.apply(0,graphs[ii]); 
-          mg.fEditable = false; 
-          mg.fTitle = titles[ii]; 
-          JSROOT.draw(canvases[ii],mg,"A" +pstyle[ii], function (painter) 
-            {
-              mg.fHistogram.fXaxis.fTitle=xtitles[ii]; 
-              mg.fHistogram.fYaxis.fTitle=ytitles[ii]; 
-              mg.fHistogram.fYaxis.fTimeDisplay=ytime[ii]; 
-              mg.fHistogram.fXaxis.fTimeDisplay=xtime[ii]; 
-              JSROOT.redraw(canvases[ii],mg,"A" +pstyle[ii], function (painter) 
-                {
-                  var leg = makeLegend(0.8,1,0.9,1,graphs[ii]); 
-                  JSROOT.draw(canvases[ii],leg);
-                  legends.push(leg); 
-                }); 
-            }
-          ); 
-          multigraphs.push(mg); 
+          page.wants[i] -= draws.length; 
+          continue; 
         }
-      }); 
+        args = { expr: draws[j], cut: cut, graph: true, drawopt: [i,j,it]}; 
+//        console.log(args); 
+        ts[it].Draw(args, function(g,indices,ignore)
+        {
+          var ii = indices[0]; 
+          var jj = indices[1]; 
+          var tt = indices[2]; 
+
+          g.fEditable = false; 
+          g.fTitle = page.labels[ii][jj]; 
+          g.fName = page.labels[ii][jj]; 
+          g.fLineColor = graph_colors[jj]; 
+          g.fMarkerColor = graph_colors[jj]; 
+          g.fFillColor = graph_colors[jj]; 
+          page.graphs[ii].push(g); 
+          if (tt == 0) 
+          {
+            page.leg_graphs[ii].push(g); 
+          }
+          if (page.graphs[ii].length == page.wants[ii]) 
+          {
+            var mg = JSROOT.CreateTMultiGraph.apply(0,page.graphs[ii]); 
+            mg.fEditable = false; 
+            mg.fTitle = page.titles[ii]; 
+            JSROOT.draw(page.canvases[ii],mg,"A" +page.pstyle[ii], function (painter) 
+              {
+                mg.fHistogram.fXaxis.fTitle=page.xtitles[ii]; 
+                mg.fHistogram.fYaxis.fTitle=page.ytitles[ii]; 
+                mg.fHistogram.fYaxis.fTimeDisplay=page.ytime[ii]; 
+                mg.fHistogram.fXaxis.fTimeDisplay=page.xtime[ii]; 
+                JSROOT.redraw(page.canvases[ii],mg,"A" +page.pstyle[ii], function (painter) 
+                  {
+                    var leg = makeLegend(0.7,1,0.9,1,page.leg_graphs[ii]); 
+                    JSROOT.draw(page.canvases[ii],leg);
+                    page.legends.push(leg); 
+                  }); 
+              }
+            ); 
+            page.multigraphs.push(mg); 
+          }
+        }); 
+      }
     }
   }
 }
 
-var HKTree = ''; 
-var HKTreeWhen = 0; 
+
+function statusTreeDraw()
+{
+  var cut = document.getElementById('status_cut').value; 
+  var run0 = parseInt(document.getElementById('status_start_run').value); 
+  var run1 = parseInt(document.getElementById('status_end_run').value); 
+
+
+  var status_trees = []; 
+  startLoading("Loading status files... be patient if you asked for a lot of runs"); 
+
+  var files_to_load = [];
+  for (var r = run0; r <= run1; r++)
+  {
+    files_to_load.push("rootdata/run"+r+"/status.root"); 
+  }
+  console.log(files_to_load); 
+
+  for (var i = 0; i < files_to_load.length; i++)
+  {
+    JSROOT.OpenFile(files_to_load[i], function(file)
+    {  
+       if (file == null)
+       { 
+         status_trees.push(null); 
+         return; 
+       }
+
+       file.ReadObject("status;1", function(tree) 
+       {
+          status_trees.push(tree); 
+          if (status_trees.length == files_to_load.length) 
+          {
+             stopLoading(); 
+             doDraw(pages['status'],status_trees,'plot_status',cut); 
+          }
+       }); 
+    }) ; 
+  }
+}
+
 
 function hkTreeDraw() 
 {
-  var cut = document.getElementById('cut').value; 
+  var cut = document.getElementById('hk_cut').value; 
   if (cut != "") cut+= "&&"; 
-  var t0 = new Date(document.getElementById('start_time').value); 
-  var t1 = new Date(document.getElementById('end_time').value); 
+  var t0 = new Date(document.getElementById('hk_start_time').value); 
+  var t1 = new Date(document.getElementById('hk_end_time').value); 
   cut += "(hk.unixTime>" + t0.getTime()/1000 + "&&hk.unixTime<" + t1.getTime()/1000 + ")"; 
 
-  if (HKTree == '' || lastModified('all_hk.root') >  HKTreeWhen) 
+  //figure out what days we need 
+
+  var hktrees = []; 
+
+  startLoading("Loading hk files"); 
+  var files_to_load = []; 
+  for (var d = new Date(t0); d<= t1; d.setDate(d.getDate()+1)) 
   {
-    startLoading("Loading all_hk.root"); 
-    var when = lastModified('all_hk.root'); 
-    JSROOT.OpenFile('all_hk.root', function(file) { 
-                    file.ReadObject("hk;1", function(tree) 
-                    {
-                      stopLoading(); 
-                      HKTree = tree; 
-                      HKTreeWhen = when; 
-                      doDraw(tree,'plot_hk',cut); 
-                    }); 
-           }
-      ) ; 
+    var mon = d.getUTCMonth()+1; 
+    var day = d.getUTCDate(); 
+    if (mon < 9) mon = "0" + mon; 
+    if (day < 9) day = "0" + day; 
+    files_to_load.push("rootdata/hk/" + d.getUTCFullYear()  + "/" + mon + "/" + day+ ".root"); 
   }
-  else doDraw(HKTree,'plot_hk',cut); 
+  console.log(files_to_load); 
+
+  for (var i = 0; i < files_to_load.length; i++)
+  {
+
+    JSROOT.OpenFile(files_to_load[i], function(file)
+    { 
+       if (file == null)
+       { 
+         hktrees.push(null); 
+         return; 
+       }
+       file.ReadObject("hk;1", function(tree) 
+       {
+          hktrees.push(tree); 
+          if (hktrees.length == files_to_load.length) 
+          {
+             stopLoading(); 
+             doDraw(pages['hk'],hktrees,'plot_hk',cut); 
+          }
+       }); 
+    }) ; 
+  }
 }
 
 
@@ -263,37 +357,145 @@ function hkTreeDraw()
 function hk() 
 {
 
-  optAppend("Start Time: <input id='start_time' size=20> ");
-  optAppend("Stop Time: <input id='end_time' size=20> " ); 
-  optAppend("Cut: <input id='cut' size=20 value='Entry$%10==0'> <br>");
+  optAppend("Start Time: <input id='hk_start_time' size=30> ");
+  optAppend("Stop Time: <input id='hk_end_time' size=30> " ); 
+  optAppend("Cut: <input id='hk_cut' size=20 value='Entry$%10==0'> <br>");
   optAppend("Plot(<a onClick='return plotHelp()' href='#hk'>?</a>):<br>");
   optAppend("<textarea id='plot_hk' cols=160 rows=5>hk.unixTime:hk.temp_master|||hk.unixTime:hk.temp_slave|||hk.unixTime:hk.temp_case;;;xtitle:time;title:Temperatures;ytitle:C;xtime:1;labels:master,slave,case\nhk.unixTime:hk.current_master|||hk.unixTime:hk.current_slave|||hk.unixTime:hk.current_frontend;;;xtitle:time;ytitle:mA;labels:master,slave,frontend;xtime:1;title:currents\nhk.unixTime:hk.disk_space_kB;;;title:disk;xtitle:time;xtime:1;labels:disk;ytitle:kB</textarea>");
   optAppend("<br><input type='button' onClick='return hkTreeDraw()' value='Draw'>"); 
-  optAppend("<a href='all_hk.root'>  (Download ROOT File)</a>"); 
+  optAppend("<a href='all_hk.root'>  (Download All HK ROOT File)</a>"); 
   
   var now = Date.now(); 
 
-  document.getElementById('start_time').value = new Date(Date.now()- 7*24*3600*1000).toISOString(); 
-  document.getElementById('end_time').value = new Date(Date.now()).toISOString(); 
+  document.getElementById('hk_start_time').value = new Date(Date.now()- 7*24*3600*1000).toISOString(); 
+  document.getElementById('hk_end_time').value = new Date(Date.now()).toISOString(); 
 
   hkTreeDraw(); 
 
 } 
 
 
+function header()
+{
+
+  optAppend("Start Run: <input id='header_start_run' size=10> ");
+  optAppend("Stop Run: <input id='header_end_run' size=10> " ); 
+  optAppend("Cut: <input id='header_cut' size=20 value='Entry$%10==0'> <br>");
+  optAppend("Plot(<a onClick='return plotHelp()' href='#status'>?</a>):<br>");
+
+  var global_scalers= "status.readout_time+status.readout_time_ns*1e-9:status.global_scalers[0]/10";
+  global_scalers += "|||status.readout_time+status.readout_time_ns*1e-9:status.global_scalers[1]/10";
+  global_scalers += "|||status.readout_time+status.readout_time_ns*1e-9:status.global_scalers[2]";
+  global_scalers += ";;;xtitle:time;title:Global Scalers;ytitle:Hz;xtime:1;labels:Slow,Slow Gated,Fast"
+
+  var beam_scalers = ""; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_scalers+="|||"; 
+    beam_scalers+="status.readout_time+status.readout_time_ns*1e-9:status.beam_scalers[0]["+i+"]/10."; 
+  }
+  beam_scalers+=";;;xtitle:time;title:Beam Scalers;ytitle:Hz;xtime:1;labels:"; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_scalers+=","; 
+    beam_scalers+="Beam "+i; 
+  }
+
+  var beam_thresholds = ""; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_thresholds+="|||"; 
+    beam_thresholds+="status.readout_time+status.readout_time_ns*1e-9:status.trigger_thresholds["+i+"]/10."; 
+  }
+  beam_thresholds+=";;;xtitle:time;title:Trigger thresholds;ytitle:Power Sum(arb);xtime:1;labels:"; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_thresholds+=","; 
+    beam_thresholds+="Beam "+i; 
+  }
+
+  optAppend("<textarea id='plot_status' cols=160 rows=5>"+global_scalers+"\n"+beam_scalers+"\n"+beam_thresholds+"</textarea>");
+  optAppend("<br><input type='button' onClick='return statusTreeDraw()' value='Draw'>"); 
+
+  document.getElementById('status_start_run').value =  runs[Math.max(0,runs.length-5)]; 
+  document.getElementById('status_end_run').value =  runs[runs.length-1]; 
+
+  statusTreeDraw(); 
+}
+function stat()
+{
+
+  optAppend("Start Run: <input id='status_start_run' size=10> ");
+  optAppend("Stop Run: <input id='status_end_run' size=10> " ); 
+  optAppend("Cut: <input id='status_cut' size=20 value='Entry$%10==0'> <br>");
+  optAppend("Plot(<a onClick='return plotHelp()' href='#status'>?</a>):<br>");
+
+  var global_scalers= "status.readout_time+status.readout_time_ns*1e-9:status.global_scalers[0]/10";
+  global_scalers += "|||status.readout_time+status.readout_time_ns*1e-9:status.global_scalers[1]/10";
+  global_scalers += "|||status.readout_time+status.readout_time_ns*1e-9:status.global_scalers[2]";
+  global_scalers += ";;;xtitle:time;title:Global Scalers;ytitle:Hz;xtime:1;labels:Slow,Slow Gated,Fast"
+
+  var beam_scalers = ""; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_scalers+="|||"; 
+    beam_scalers+="status.readout_time+status.readout_time_ns*1e-9:status.beam_scalers[0]["+i+"]/10."; 
+  }
+  beam_scalers+=";;;xtitle:time;title:Beam Scalers;ytitle:Hz;xtime:1;labels:"; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_scalers+=","; 
+    beam_scalers+="Beam "+i; 
+  }
+
+  var beam_thresholds = ""; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_thresholds+="|||"; 
+    beam_thresholds+="status.readout_time+status.readout_time_ns*1e-9:status.trigger_thresholds["+i+"]/10."; 
+  }
+  beam_thresholds+=";;;xtitle:time;title:Trigger thresholds;ytitle:Power Sum(arb);xtime:1;labels:"; 
+  for (var i = 0; i <15; i++)
+  {
+    if (i > 0) beam_thresholds+=","; 
+    beam_thresholds+="Beam "+i; 
+  }
+
+  optAppend("<textarea id='plot_status' cols=160 rows=5>"+global_scalers+"\n"+beam_scalers+"\n"+beam_thresholds+"</textarea>");
+  optAppend("<br><input type='button' onClick='return statusTreeDraw()' value='Draw'>"); 
+
+  document.getElementById('status_start_run').value =  runs[Math.max(0,runs.length-5)]; 
+  document.getElementById('status_end_run').value =  runs[runs.length-1]; 
+
+  statusTreeDraw(); 
+}
+
+
 function show(what) 
 {
   window.location.hash = what; 
   optClear(); 
-  clearCanvases(); 
+
+  for (var key in pages)
+  { 
+    document.getElementById(pages[key].main_canvas).style.display = (key === what ? 'block' : 'none');
+  }
+
+//  console.log("show('" + what + "')"); 
+  if (what in pages)
+    clearCanvases(pages[what]); 
 
   if (what == 'hk') 
   {
     hk(); 
   }
+  else if (what == 'status') 
+  {
+    stat(); 
+  }
   else
   {
-    optAppend("Not implemented yet"); 
+    optAppend("Not implemented yet");  
   }
 }
 
@@ -303,6 +505,9 @@ function monutor_load()
 
   JSROOT.gStyle.fTitleX=0.1; 
   JSROOT.gStyle.fFrameFillColor=37; 
+  pages['hk'] = Page('hk'); 
+  pages['status'] = Page('status'); 
+  pages['event'] = Page('event'); 
 
   var hash = window.location.hash; 
   window.location.hash = ''; 
