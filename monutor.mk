a6f2665b3feaf904df17edae479c965123d7d807
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-09-20 12:55:04 -0500

    actually make the json

--- monutor.mk
+++ monutor.mk
@@ -212,7 +212,7 @@ $(HTML_DIR)/jsroot: jsroot/scripts jsroot/style
 	cp -r $^ $@
 
 
-deploy:  extract rootify filtered $(HTML_DIR)/rootdata $(HTML_DIR)/index.html $(HTML_DIR)/monutor.js  $(HTML_DIR)/runlist.js $(HTML_DIR)/all_hk.root $(HTML_DIR)/jsroot $(HTML_DIR)/monutor.ico $(HTML_DIR)/monutor.png $(HTML_DIR)/KissFFT.js $(HTML_DIR)/FFT.js | $(HTML_DIR) 
+deploy:  extract rootify filtered $(HTML_DIR)/rootdata $(HTML_DIR)/index.html $(HTML_DIR)/monutor.js  $(HTML_DIR)/runlist.js $(HTML_DIR)/runlist.json $(HTML_DIR)/all_hk.root $(HTML_DIR)/jsroot $(HTML_DIR)/monutor.ico $(HTML_DIR)/monutor.png $(HTML_DIR)/KissFFT.js $(HTML_DIR)/FFT.js | $(HTML_DIR) 
 	touch $@ 
 
 
